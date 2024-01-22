5061d2cf0f934f74ed4b754967b71caf3f91e2eb
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-22 19:34:02 +0000

    added comments for test clarity

--- tests/test-yarn.rb
+++ tests/test-yarn.rb
@@ -2,11 +2,13 @@ require 'minitest/autorun'
 require './store'
 
 class TestYarn < Minitest::Test
+  #test that yarn "cart" is empty when store is first opened
   def test_new_store
     store=Store.new
     assert_equal "There is no yarn in your cart currently.", store.list_yarns
   end
-
+  
+  #test to make sure that adding yarn to "cart" works
   def test_add_yarn
     store=Store.new
     assert_equal "Yarn added to cart!", store.add_yarn
