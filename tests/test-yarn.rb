85187bd48c4467d8b1a525af0ca65249816c6e3f
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-22 19:19:57 +0000

    changing requires

--- tests/test-yarn.rb
+++ tests/test-yarn.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
-require './store'
+require './app'
 
 class TestYarn < Minitest::Test
   def test_new_store
     store=Store.new
     assert_equal yarns, store.list_yarns
   end
-  
+
 end
